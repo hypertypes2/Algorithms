3a08f0ff7686037ee8cd59da3d6e6484a77b669b
hypertypes2 <132851688+hypertypes2@users.noreply.github.com>
2023-05-22 19:29:11 +0900

    [Gold V] Title: 토마토, Time: 1512 ms, Memory: 99644 KB -BaekjoonHub

---
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -0,0 +1,45 @@
+from collections import deque
+m,n=map(int,input().split())
+graph=[[0 for col in range(m)] for row in range(n)]
+for i in range(n):
+    S=list(map(int,input().split()))
+    for j in range(m):
+        graph[i][j]=S[j]
+        
+di=[0,1,0,-1]
+dj=[1,0,-1,0]
+flag=True
+
+que=deque()
+for i in range(n):
+    for j in range(m):
+        if graph[i][j]==1:
+            que.append((i,j))
+            
+def BFS():
+    while que:
+        i,j=que.popleft()
+        for o in range(4):
+            ni=i+di[o]
+            nj=j+dj[o]
+            if 0<=ni<n and 0<=nj<m and graph[ni][nj]==0:
+                que.append((ni,nj))
+                graph[ni][nj]=graph[i][j]+1   
+                                 
+
+BFS()
+
+for i in range(n):
+    for j in range(m):
+        if graph[i][j]==0:
+            flag=False
+if not flag:
+    print(-1)
+else:
+    t=[]
+    for g in graph:
+        g.sort()
+    for sg in graph:
+        t.append(sg[-1])        
+        
+    print(max(t)-1)
