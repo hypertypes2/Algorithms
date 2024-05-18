d65dd7ef7e86b2039bee3416ab952476bb2ce080
hypertypes2 <132851688+hypertypes2@users.noreply.github.com>
2024-05-18 14:19:24 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.34 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -1,16 +1,11 @@
 def solution(n):
     if n<3:
         return n
-    dp=[0]*(n+1)
-    dp[1],dp[2]=1,2
     
-    def fibo(x):
-        nonlocal dp
-        for x in range(3,n+1):
-            dp[x]=(dp[x-1]+dp[x-2])%1234567
-    
-    fibo(n)
-    
-    return dp[n]
-        
-    
+    d = [0]*(n+1)
+    d[1]=1
+    d[2]=2
+
+    for i in range(3,n+1):
+        d[i]=(d[i-1]+d[i-2])%1234567
+    return d[n]
