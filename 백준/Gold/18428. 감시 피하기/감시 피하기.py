f7bb24874fd0cfde4097838c14f325e98bbc99c2
hypertypes2 <132851688+hypertypes2@users.noreply.github.com>
2024-05-22 23:19:10 +0900

    [Gold V] Title: 감시 피하기, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/18428. 감시 피하기/감시 피하기.py
@@ -0,0 +1,54 @@
+n = int(input())
+map = [(input().split()) for _ in range(n)]
+
+empty = []
+teach = []
+stude = []
+
+ans = 'NO'
+dx = [1,0,-1,0]
+dy = [0,1,0,-1]
+
+for i in range(n):
+    for j in range(n):
+        if map[i][j] == 'X':
+            empty.append((i,j))
+        elif map[i][j] == 'T':
+            teach.append((i,j))
+        else:
+            stude.append((i,j))
+            
+def DFS(wall, idx):
+    global ans
+    # if ans == 'YES':
+    #     return 
+    if len(wall) == 3:
+        for i,j in wall:
+            map[i][j] = 'O'
+        for x,y in teach:
+            for o in range(4):
+                for k in range(1,n+1):
+                    nx = x + dx[o] * k
+                    ny = y + dy[o] * k
+                    if nx<0 or nx>=n or ny<0 or ny>=n:
+                        break
+                    if map[nx][ny] == 'O':
+                        break
+                    if map[nx][ny] == 'S':
+                        for i,j in wall:
+                            map[i][j] = 'X'
+                        return
+        #ans = 'YES'
+        print('YES')
+        exit(0)
+        return      
+    
+    for i in range(idx,len(empty)):
+        wall.append(empty[i])
+        DFS(wall, i+1)
+        wall.pop()
+
+
+DFS([],0)
+
+print(ans)
