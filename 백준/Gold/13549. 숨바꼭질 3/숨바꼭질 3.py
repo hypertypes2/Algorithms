b7cf094af9dd12dd1f719db3d6dfb582b0087fb3
hypertypes2 <132851688+hypertypes2@users.noreply.github.com>
2024-05-29 22:56:05 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 112 ms, Memory: 36004 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -1,28 +1,29 @@
 from collections import deque
-n,k=map(int,input().split())
-limit=100001
-cnt=[0]*limit
-visited=[False]*limit
-def bfs(x,end):
-    queue=deque()
-    queue.append(x)
 
-    while queue:
-        x=queue.popleft()
-        if x==end:return cnt[x]
-        if -1<x*2<limit and visited[x*2]==0 :
-            queue.appendleft(x*2)
-            cnt[x*2]=cnt[x]
-            visited[x*2]=True
-        if -1<x-1<limit and visited[x-1]==0 :
-            queue.append(x-1)
-            cnt[x-1]=cnt[x]+1
-            visited[x-1]=True
-        if -1<x+1<limit and visited[x+1]==0 :
-            queue.append(x+1)
-            cnt[x+1]=cnt[x]+1
-            visited[x+1]=True
+n,k = map(int,input().split())
+MAXN = 100000
 
+visit = [False] * (MAXN+1)
+cnt = [0] * (MAXN+1)
 
-
-print(bfs(n,k))
+def BFS(start):
+    que = deque()
+    que.append(start)
+    while que:
+        x = que.popleft()
+        if x == k:
+            print(cnt[k])
+            return
+        for w,nx in enumerate([2*x, x-1, x+1]):
+            if 0>nx or nx>MAXN or visit[nx]:
+                continue
+                
+            if w == 0:
+                cnt[nx] = cnt[x]
+                que.appendleft(nx)
+                visit[nx] = True
+            else:
+                cnt[nx] = cnt[x] + 1
+                que.append(nx)
+                visit[nx] = True
+BFS(n)
